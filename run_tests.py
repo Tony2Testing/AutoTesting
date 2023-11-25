f82c4383dceef0e3325671e266d89e5dba90544f
yuyong <173391618@qq.com>
2023-11-25 19:49:02 +0800

    first commit

---
+++ run_tests.py
@@ -0,0 +1,30 @@
+import unittest
+from BSTestRunner import BSTestRunner
+import os
+
+def main():
+    # 测试用例的目录
+    case_path = "./test_cases"
+
+    # 测试报告的目录
+    report_path = "./test_reports"
+    if not os.path.exists(report_path):
+        os.makedirs(report_path)
+
+    # 测试报告文件名固定为 report.html
+    report_file = os.path.join(report_path, "report.html")
+
+    # 加载测试用例
+    discover = unittest.defaultTestLoader.discover(case_path, pattern="test*.py")
+
+    # 运行测试用例并生成报告
+    with open(report_file, "wb") as report:
+        runner = BSTestRunner(
+            stream=report,
+            title="测试报告",
+            description="测试报告明细"
+        )
+        runner.run(discover)
+
+if __name__ == "__main__":
+    main()
---
+++ test_cases/test_baidu.py
@@ -0,0 +1,61 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class TestBaiduSearch(unittest.TestCase):
+
+    def setUp(self):
+        # 前置条件: 用户打开百度搜索页面
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.baidu.com")
+
+    def test_search_valid_keyword(self):
+        # 测试步骤:
+        # 1. 在搜索输入框中输入一个有效关键字（例如："测试"）
+        search_input = self.driver.find_element(By.ID, "kw")
+        search_input.send_keys("测试")
+
+        # 2. 点击搜索按钮
+        search_button = self.driver.find_element(By.ID, "su")
+        search_button.click()
+
+        sleep(2)
+        # 预期结果: 显示搜索结果页面。
+        self.assertIn("百度为您找到相关结果约", self.driver.page_source)
+
+    def test_search_multiple_valid_keywords(self):
+        # 测试步骤:
+        # 1. 在搜索输入框中输入多个有效关键字（例如："软件测试方法"）
+        search_input = self.driver.find_element(By.ID, "kw")
+        search_input.send_keys("软件测试方法")
+
+        # 2. 点击搜索按钮
+        search_button = self.driver.find_element(By.ID, "su")
+        search_button.click()
+
+        sleep(2)
+        # 预期结果: 显示搜索结果页面。
+        self.assertIn("百度为您找到相关结果约", self.driver.page_source)
+
+    def test_search_special_characters(self):
+        # 测试步骤:
+        # 1. 在搜索输入框中输入特殊字符（例如："#$%^"）
+        search_input = self.driver.find_element(By.ID, "kw")
+        search_input.send_keys("#$%^")
+
+        # 2. 点击搜索按钮
+        search_button = self.driver.find_element(By.ID, "su")
+        search_button.click()
+
+        sleep(2)
+        # 预期结果: 显示错误消息提示。
+        error_message = self.driver.find_element(By.CLASS_NAME, "nors").text
+        self.assertEqual(error_message, "抱歉，没有找到与“#$%^”相关的结果。")
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
